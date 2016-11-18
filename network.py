362b9f297738757b76452108c3836f423d1abc9e
Jussi Martin <jussi.h.a.martin@gmail.com>
2016-11-18 16:40:46 +0200

    Added methods backprop and cost_derivative

--- network.py
+++ network.py
@@ -2,7 +2,12 @@ import random
 import numpy as np
 
 def sigmoid(z):
+    """The sugmoid function."""
     return 1.0 / (1.0 + np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function"""
+    return sigmoid(z) * (1 - sigmoid(z))
 
 class Network(object):
 
@@ -57,3 +62,42 @@ class Network(object):
                         for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta / len(mini_batch))*nb
                         for b, nb in zip(self.biases, nabla_b)]
+
+    def backprop(self, x, y):
+        """Return a tuple "(nabla_b, nabla_w)" representing the
+        gradient for the cost function C_x.  "nabla_b" and
+        "nabla_w" are layer-by-layer lists if numpy arrays, similar
+        to "self.biases" and "self.weights"."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        # feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b, w zip(self.biases, self.weights):
+            z = np.dot(w, activations) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        # backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].tranpose())
+        # Note that the variable l in the loop below is used a little
+        # differently to the notation in Chapter 2 the book.  Here,
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # shceme in the book, used here to take advantage of the fact
+        # that Python can use negative indicies in lists.
+        for l in xrange(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l + 1].transpose() delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+        return (nabla_b, nabla_w)
+
+        def cost_derivative(self, output_activations, y):
+            """Return the vector of partial derivatives \partial C_x /
+            \patial a for the output activations."""
+            return (output_activations - y)
